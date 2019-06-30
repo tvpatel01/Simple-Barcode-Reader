12cf3f7e55acced4d4d07b1f9d4280d3feb138da
Taral <tvpatel01@gmail.com>
2019-06-30 18:52:43 -0400

    Added barcode_scan library support

---
+++ lib/driver.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Driver {
+
+  final String firstName;
+  final String surName;
+  final String dob;
+  final String addrLine1;
+  final String addrLine2;
+
+  Driver(
+    {@required this.firstName,
+      @required this.surName,
+      @required this.dob,
+      @required this.addrLine1,
+      @required this.addrLine2});
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'driver.dart';
+import 'results.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'iVerify Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.lightGreen,
+
+        primarySwatch: Colors.amber,
       ),
       home: MyHomePage(title: 'i-Verify'),
     );
   }
 }
-
+ 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -45,50 +31,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String barcode = "";
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -104,8 +62,59 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.camera_enhance),
+      ),
     );
+  }
+
+  Future scan(context) async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      print('entered barcode scan');
+      var txtSplit = barcode.split('\n');
+      print('barcode : ' + barcode);
+      print(txtSplit);
+      for (int i = 0; i < txtSplit.length; i++) {
+        print('${txtSplit[i]}');
+      }
+
+      String surName = txtSplit[5].substring(3);
+      String firstName = txtSplit[6].substring(3);
+      String addrLine1 = txtSplit[12].substring(3);
+      String addrLine2 = txtSplit[13].substring(3);
+      String addrLine3 = txtSplit[14].substring(3);
+      String addrLine4 = txtSplit[15].substring(3);
+      String addrRest = addrLine2 + ' ' + addrLine3 + ' ' + addrLine4;
+      String dob = txtSplit[8].substring(3);
+      String dobFormatted = dob.substring(4, 8) +
+          '-' +
+          dob.substring(0, 2) +
+          '-' +
+          dob.substring(2, 4);
+      Driver driver = new Driver(
+          firstName: firstName,
+          surName: surName,
+          dob: dobFormatted,
+          addrLine1: addrLine1,
+          addrLine2: addrRest);
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (BuildContext context) => Results(driver)));
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'The user did not grant the camera permission!';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    } on FormatException {
+      setState(() => this.barcode =
+          'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
   }
 }
